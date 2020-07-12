900f1ebc4c4930313ad8f664031e4dd25356f5d2
raha <elahe.dstn@gmail.com>
2020-07-12 15:19:00 +0430

    fix:lint issues

--- cmd/root.go
+++ cmd/root.go
@@ -13,6 +13,7 @@ import (
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
+//nolint: gofumpt
 func Execute() {
 	var rootCmd = &cobra.Command{
 		Use:   "monitor",
