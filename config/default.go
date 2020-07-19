67ec8d0e4639455ea5f75bdf2ac6065b872498ec
raha <elahe.dstn@gmail.com>
2020-07-19 16:26:26 +0430

    test APIs

--- config/default.go
+++ config/default.go
@@ -3,14 +3,14 @@ package config
 const Default = `
 db:
   host: 127.0.0.1
-  port: "5431"
+  port: "5432"
   user: postgres
   dbname: monitor
   password: postgres
   sslmode: disable
 redis:
   host: 127.0.0.1
-  port: "6378"
+  port: "6379"
   threshold: 4
 jwt:
   secret: jdnfksdmfks
