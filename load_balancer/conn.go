8974e8b1c7e82fac6add2f64711a949f93d6c110
raha <elahe.dstn@gmail.com>
2020-07-23 21:02:35 +0430

    Get conn to nats

--- load_balancer/conn.go
+++ load_balancer/conn.go
@@ -1,1 +1,17 @@
 package load_balancer
+
+import (
+	"log"
+
+	"github.com/elahe-dastan/HTTP_monitoring/config"
+	"github.com/nats-io/go-nats"
+)
+
+func Conn(n config.Nats) *nats.Conn{
+	nc, err := nats.Connect(n.Host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return nc
+}
